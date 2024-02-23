c439e9a3770c1ee870469b5a8495d43abb05a9c4
kanandha <kanandha@applab.qa>
2024-02-23 17:30:28 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,239 +1,29 @@
-// import React from 'react';
-// import Notifications from './components/Notifications';
-// import Options from './components/Options';
-// import VideoPlayer from './components/VideoPlayer';
-// import NavBar from './components/NavBar';
-// import About from './components/About';
-// import Users from './components/Users';
-
-// const App = () => {
-//     return (
-//         <div>
-//             {/* <NavBar /> */}
-//             <Options>
-//                 <Notifications />
-//             </Options>
-//             <VideoPlayer />
-//             {/* <Users />
-//             <About /> */}
-//         </div>
-//     );
-// }
-
-// export default App;
-
-import React, { createContext, useState, useRef, useEffect } from 'react';
-
-import { io } from 'socket.io-client';
-import Peer from 'simple-peer';
-
+import React from 'react';
 import Notifications from './components/Notifications';
 import Options from './components/Options';
 import VideoPlayer from './components/VideoPlayer';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import NavBar from './components/NavBar';
 import About from './components/About';
 import Users from './components/Users';
-
+import VideoCall from './createRoom';
+import Room from './room';
 
 const App = () => {
-
-    const [yourID, setYourID] = useState("");
-    // const [users, setUsers] = useState({});
-    const [callAccepted, setCallAccepted] = useState(false);
-    const [callEnded, setCallEnded] = useState(false);
-    const [callMuted, setCallMuted] = useState(false);
-    const [stream, setStream] = useState();
-    const [name, setName] = useState('');
-    const [call, setCall] = useState({});
-    const [me, setMe] = useState('');
-    const [idToCall, setIdToCall] = useState('');
-
-    const [callEndedByUser, setCallEndedByUser] = useState(false);
-
-    // console.log(callEndedByUser1,"callEndedByUser1")
-
-    const myVideo = useRef();
-    const userVideo = useRef();
-    const connectionRef = useRef();
-    const socket = useRef();
-
-
-    useEffect(() => {
-        socket.current = io.connect("http://localhost:8000");
-
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-            .then((currentStream) => {
-                setStream(currentStream);
-                myVideo.current.srcObject = currentStream;
-            });
-
-        socket.current.on('me', (id) => setMe(id));
-
-        socket.current.on("yourID", (id) => {
-            setYourID(id);
-        });
-
-        socket.current.on('callUser', ({ from, name: callerName, signal }) => {
-            setCall({ isReceivingCall: true, from, name: callerName, signal });
-        });
-
-        socket.current.on("callEnded", () => {
-            setCallEnded(true)
-            // setCallAccepted(false);
-            // setCall({}); // Reset call state
-            connectionRef.current.destroy();
-
-            window.location.reload();
-            console.log("The call has ended");
-        });
-
-        return () => {
-            socket.current.disconnect();
-        };
-
-    }, []);
-
-
-    const answerCall = () => {
-        setCallAccepted(true);
-
-        const peer = new Peer({ initiator: false, trickle: false, stream });
-
-        peer.on('signal', (data) => {
-            socket.current.emit('answerCall', { signal: data, to: call.from });
-        });
-
-        peer.on('stream', (currentStream) => {
-            userVideo.current.srcObject = currentStream;
-        });
-
-        peer.signal(call.signal);
-
-        connectionRef.current = peer;
-    };
-
-    const callUser = (id) => {
-        const peer = new Peer({ initiator: true, trickle: false, stream });
-
-        peer.on('signal', (data) => {
-            socket.current.emit('callUser', { userToCall: id, signalData: data, from: me, name });
-        });
-
-        peer.on('stream', (currentStream) => {
-
-            userVideo.current.srcObject = currentStream;
-        });
-
-        socket.current.on('callAccepted', (signal) => {
-            setCallAccepted(true);
-            peer.signal(signal);
-        });
-
-        connectionRef.current = peer;
-    };
-
-    const leaveCall = () => {
-        setCallEnded(true);
-        connectionRef.current.destroy();
-        socket.current.emit("callEnded", true); // Notify the server that the call has ended
-
-        window.location.reload();
-        // setUsers(users);
-
-    };
-
-    const muteCall = () => {
-        setCallMuted(true);
-    };
-
-    const unMuteCall = () => {
-        setCallMuted(false);
-    }
-
-
-    // console.log(userVideo.current===undefined)
-
-
-
     return (
         <div>
-
             {/* <NavBar /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" exact element={<VideoCall />} />
+                    {/* <Route path="/" element={<Room />} /> */}
 
-            <div>
-                <div className='options'>
-                    <div className='option'>
-                        <form noValidate autoComplete='off'>
-                            <label>Set Your Name to be Displayed</label><br />
-                            <input type='text' label='Name' value={name} onChange={(e) => setName(e.target.value)} />
-                        </form>
-                        <br />
-                        <h3>Your ID: {me}</h3>
-                        <p>Share this with your Friends so they can call you</p>
-                    </div>
-                    <div className='option'>
-                        <form noValidate autoComplete='off'>
-                            <label>Make A Call</label><br />
-                            <input type='text' label='ID to call' value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
-                        </form>
-                        <br />
-                        {
-                            callAccepted && !callEnded ?   (
-                                <button onClick={leaveCall}>Hang Up</button>
-                            ) : (
-                                <button onClick={() => callUser(idToCall)}>Call</button>
-                            )}
-                    </div>
-                </div>
+                </Routes>
+            </BrowserRouter>
 
-                <>
-                    {call.isReceivingCall && !callAccepted && (
-                        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                            <h1>{call.name} is calling:</h1>
-                            <button onClick={answerCall}>
-                                Answer
-                            </button>
-                        </div>
-                    )}
-                </>
-            </div>
 
-            <div className='videos' id='video'>
-
-                {
-                    stream && (
-                        <div>
-                            <div className='floatingName'>
-                                <h3>{name || 'Name'}</h3>
-                            </div>
-                            <video playsInline muted ref={myVideo} autoPlay style={{ height: "50px", width: "50px" }}></video>
-                        </div>
-                    )
-                }
-
-                {
-                    callAccepted && !callEnded&&
-                    (
-                        <div>
-                            <div className='floatingName'>
-                                <h3>{call.name || 'Name'}</h3>
-                            </div>
-                            <video playsInline muted={callMuted} autoPlay ref={userVideo} className='Video' style={{ height: "50px", width: "50px" }}> </video>
-                            {
-                                !callMuted ? (
-                                    <button onClick={muteCall} > Mute </button>
-                                ) : (
-                                    <button onClick={unMuteCall} > Unmute </button>
-                                )
-                            }
-                        </div>
-                    )
-                }
-            </div>
-            {/* <Options>
-                <Notifications />
-            </Options> */}
-            {/* <VideoPlayer /> */}
             {/* <Users />
             <About /> */}
         </div>
@@ -241,47 +31,3 @@ const App = () => {
 }
 
 export default App;
-
-
-
-// useEffect(() => {
-//     // socket.current = io.connect("https://video-chat-7w68.onrender.com");
-//     socket.current = io.connect("http://localhost:8000");
-
-//         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-//             .then((currentStream) => {
-//                 setStream(currentStream);
-//                 myVideo.current.srcObject = currentStream;
-//             });
-
-
-//     socket.current.on('me', (id) => setMe(id));
-
-//     socket.current.on("yourID", (id) => {
-//         setYourID(id);
-
-//     });
-
-
-//     // socket.current.on("allUsers", (users) => {
-//     //     setUsers(users);
-//     // });
-
-
-//     socket.current.on('callUser', ({ from, name: callerName, signal }) => {
-//         setCall({ isReceivingCall: true, from, name: callerName, signal });
-
-//     });
-
-//     socket.current.on("callEnded", () => {
-//         setCallEnded(true);
-//         connectionRef.current.destroy();
-
-//         window.location.reload();
-
-//         // Handle the call ended event here
-//         console.log("The call has ended");
-//         // You can perform any other actions here, such as updating UI
-//     });
-
-// }, []);
--- express/index.js
+++ express/index.js
@@ -5,13 +5,13 @@ const cors = require("cors");
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
-		methods: [ "GET", "POST" ]
+		methods: ["GET", "POST"]
 	}
 });
 
 app.use(cors());
 
-const PORT =  8000;
+const PORT = 8000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
@@ -20,86 +20,55 @@ app.get('/', (req, res) => {
 
 const users = {};
 
+const activeCalls = new Map();
+
+
 io.on("connection", (socket) => {
 	if (!users[socket.id]) {
-        users[socket.id] = socket.id;
-    }
+		users[socket.id] = socket.id;
+	}
 	socket.emit("yourID", socket.id);
 	socket.emit("me", socket.id);
-	socket.emit("callDrop", true);
 
 
 	io.sockets.emit("allUsers", users);
 
-	// socket.on("disconnect", () => {
-	// 	socket.broadcast.emit("callEnded",true)
-	// 	delete users[socket.id];
+	// socket.on("callEnded", ({user}) => {
+	// 	io.emit("callEnded",{user})
+
+	// 	// socket.broadcast.emit("callEnded",true)
+	// 	delete users[user];
 	// });
+
+	socket.on("callEnded", (data) => {
+		const { user, otherUser } = data;
+		io.to(otherUser).emit("callEnded", { user });
+		delete users[otherUser];
+
+	});
+
+
+
 
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
 	});
 
-	
+
 	socket.on("answerCall", (data) => {
 		io.to(data.to).emit("callAccepted", data.signal)
 	});
 
-	// socket.on("callEnded", () => {
-    //     io.emit("callEnded"); // Broadcast a "callEnded" message to all connected clients
-    // });
+	// socket.on("callEnded", ({user}) => {
+	//     io.to(user).emit("callEnded",user ); // Broadcast a "callEnded" message to all connected clients
+	// });
 
-	socket.on("callEnded", () => {
-		socket.broadcast.emit("callEnded"); // Broadcast a "callEnded" message to all connected clients except the sender
-	});
+	// socket.on("callEnded", () => {
+	// 	socket.broadcast.emit("callEnded"); // Broadcast a "callEnded" message to all connected clients except the sender
+	// });
 
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-// const app = require("express")();
-// const server = require("http").createServer(app);
-// const cors = require("cors");
 
-// const io = require("socket.io")(server, {
-// 	cors: {
-// 		origin: "*",
-// 		methods: [ "GET", "POST" ]
-// 	}
-// });
-
-// app.use(cors());
-
-// const PORT = process.env.PORT || 8000;
-
-// app.get('/', (req, res) => {
-// 	res.send('Running');
-// });
-
-
-// const users = {};
-
-// io.on("connection", (socket) => {
-// 	if (!users[socket.id]) {
-//         users[socket.id] = socket.id;
-//     }
-// 	socket.emit("yourID", socket.id);
-// 	socket.emit("me", socket.id);
-
-// 	io.sockets.emit("allUsers", users);
-
-// 	socket.on("disconnect", () => {
-// 		socket.broadcast.emit("callEnded")
-// 		delete users[socket.id];
-// 	});
-
-// 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-// 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-// 	});
-
-// 	socket.on("answerCall", (data) => {
-// 		io.to(data.to).emit("callAccepted", data.signal)
-// 	});
-// });
-
-// server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
